d3d210336bdcb52377a48ba3ef653ecd67b63988
Shreshth Pratap Singh <114429628+Shreshth-07@users.noreply.github.com>
2022-11-06 03:21:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-let loc = document.getElementById("location");
+let loc = document.getElementById("state");
 let tempicon = document.getElementById("temp-icon");
 let tempvalue = document.getElementById("temp-value");
 let climate = document.getElementById("climate");
-let iconfile;
+let hum =document.getElementById("humi");
+let win = document.getElementById("wind");
+let con = document.getElementById("coun");
+let rise = document.getElementById("sunrise");
+let set = document.getElementById("sunset");
 const searchInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-button");
 const now = new Date();
 var hour = now.getHours();
 var min = now.getMinutes();
 var ampm = hour>=12 ? "PM" : "AM";
-//hour = hour%12;
-//hour = hour ? hour : 12;
 min = min<10 ? '0'+min : min;
+
              
 
 searchButton.addEventListener('click',(e)=>
@@ -25,17 +28,27 @@ searchButton.addEventListener('click',(e)=>
 const getWeather = async(city)=>
 {
     
+    
     try{
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=ba98deab6ce9cd74af9735fb0625c520`);
 
         const weatherData = await response.json();
         console.log(weatherData);
-        const{name} = weatherData;
-        const{feels_like} = weatherData.main;
-        const{id,main} = weatherData.weather[0];
-        loc.textContent=name;
+
+        const{name}=weatherData;
+        const{country}=weatherData.sys;
+        const{feels_like}=weatherData.main;
+        const{id,main}=weatherData.weather[0];
+        const{humidity}=weatherData.main;
+        const{speed}=weatherData.wind;
+
+        loc.textContent = name;
+        con.textContent = country;
         climate.textContent=main;
-        tempvalue.textContent=Math.round(feels_like+0);
+        tempvalue.textContent = feels_like;
+        hum.textContent = "Humidity : "+humidity+"%";
+        win.textContent = "Wind Speed : "+speed +" km/h";
+        
 
         if(id<300 && id>200)
         {
@@ -120,9 +133,7 @@ const getWeather = async(city)=>
             {
                 tempicon.src="./icons/night-cloud-icon.svg"
             }
-        }
-
-                    
+        }           
 
     }
     catch(error)
@@ -152,12 +163,23 @@ window.addEventListener("load" ,()=>
             .then(data=>
                 {
                     const{name}=data;
+                    const{country}=data.sys;
                     const{feels_like}=data.main;
                     const{id,main}=data.weather[0];
+                    const{humidity}=data.main;
+                    const{speed}=data.wind;
+                    const{sunrise}=data.sys;
+                    const{sunset}=data.sys;
 
                     loc.textContent = name;
+                    con.textContent = country;
                     climate.textContent=main;
-                    tempvalue.textContent = Math.round(feels_like+0);
+                    tempvalue.textContent =feels_like;
+                    hum.textContent = "Humidity : "+humidity+"%";
+                    win.textContent = "Wind Speed : "+speed +" km/h";
+                    rise.textContent = "Sun-rise : "+window.moment(data.sys.sunrise*1000).format('hh:mm a');
+                    set.textContent = "Sun-set : "+ window.moment(data.sys.sunset*1000).format('hh:mm a');
+                    
 
                     if(id<300 && id>200)
                     {
